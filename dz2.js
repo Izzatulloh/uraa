5c338444fff03d0986386fd23deca8354ca5c40d
Izzatulloh <Izzatulloh5566@gmail.com>
2020-06-22 00:32:39 +0500

    new

--- dz2.js
+++ dz2.js
@@ -6,33 +6,33 @@ function math(){
     switch (key) {
         case 1:question_1=+prompt(number+'+'+secnumber);
         if(question_1==number+secnumber){
-            console.log(number+secnumber);
+            console.log("Siznign javob to'g'ri - "+(number+secnumber));
         } else{
-            console.log("Sizning javob noto'gri = "+question_1+"   To'g'ri javob = "+(number+secnumber));
+            console.log("Sizning javob noto'gri - "+question_1+"   To'g'ri javob - "+(number+secnumber));
             
         }
             break;
          case 2:question_2=+prompt(number+'-'+secnumber);
          if(question_2==number-secnumber){
-            console.log(number-secnumber);
+            console.log("Siznign javob to'g'ri - "+(number-secnumber));
         } else{
-            console.log("Sizning javob noto'gri = "+question_2+"  To'g'ri javob = "+(number-secnumber));
+            console.log("Sizning javob noto'gri - "+question_2+"  To'g'ri javob - "+(number-secnumber));
             
         }
             break;
         case 3:question_3=+prompt(number+'*'+secnumber);
         if(question_3==number*secnumber){
-            console.log(number*secnumber);
+            console.log("Siznign javob to'g'ri - "+(number*secnumber));
         } else{
-            console.log("Sizning javob noto'gri = "+question_3+"  To'g'ri javob = "+(number*secnumber));
+            console.log("Sizning javob noto'gri - "+question_3+"  To'g'ri javob - "+(number*secnumber));
             
         }
             break;
         case 4: question_4=+prompt(number+'/'+secnumber);
         if(question_4==number/secnumber){
-            console.log(number/secnumber);
+            console.log("Siznign javob to'g'ri - "+(number/secnumber));
         } else{
-            console.log("Sizning javob noto'gri = "+question_4+"  To'g'ri javob = "+(number/secnumber));
+            console.log("Sizning javob noto'gri - "+question_4+"  To'g'ri javob - "+(number/secnumber));
             
         };
             break;
