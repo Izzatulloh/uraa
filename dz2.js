0d34b52b0447f9b66cb7d81c958c79dd5b2d70f6
Izzatulloh <Izzatulloh5566@gmail.com>
2020-06-22 00:21:59 +0500

    assa

--- dz2.js
+++ dz2.js
@@ -1,15 +1,43 @@
 let example = +prompt("misol sonini kiriting");
 function math(){
-    let number = Math.round(Math.random()*10)
-    let secnumber= Math.round(Math.random()*10)
-    let symbol = " + "
-    let misol = (number+secnumber)
-    return misol
-
+    let key = Math.ceil(Math.random()*4)
+    let number = Math.floor(Math.random()*11)
+    let secnumber= Math.floor(Math.random()*11)
+    switch (key) {
+        case 1:question_1=+prompt(number+'+'+secnumber);
+        if(question_1==number+secnumber){
+            console.log(number+secnumber);
+        } else{
+            console.log("Sizning javob noto'gri = "+question_1+"   To'g'ri javob = "+(number+secnumber));
+            
+        }
+            break;
+         case 2:question_2=+prompt(number+'-'+secnumber);
+         if(question_2==number-secnumber){
+            console.log(number-secnumber);
+        } else{
+            console.log("Sizning javob noto'gri = "+question_2+"  To'g'ri javob = "+(number-secnumber));
+            
+        }
+            break;
+        case 3:question_3=+prompt(number+'*'+secnumber);
+        if(question_3==number*secnumber){
+            console.log(number*secnumber);
+        } else{
+            console.log("Sizning javob noto'gri = "+question_3+"  To'g'ri javob = "+(number*secnumber));
+            
+        }
+            break;
+        case 4: question_4=+prompt(number+'/'+secnumber);
+        if(question_4==number/secnumber){
+            console.log(number/secnumber);
+        } else{
+            console.log("Sizning javob noto'gri = "+question_4+"  To'g'ri javob = "+(number/secnumber));
+            
+        };
+            break;
+    }   return key
 }  
-
-
 for(let i = 0; i < example; i++){
-   
-    console.log(math());
+    (math());
 }
