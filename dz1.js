035e0687632a741ac6f4aafac7978afe23f50fb6
Izzatulloh <Izzatulloh5566@gmail.com>
2020-06-21 11:51:51 +0500

    assas

--- dz1.js
+++ dz1.js
@@ -1,9 +1,9 @@
-// function age(){
-//     let name= prompt('Isminginzni kiriting')
-//     let now = +prompt('hozirgi yilni kiriting')
-//     let old = +prompt ("tug'ilgan yilingiz")
-//     let age = now - old;
-//     return name + '  Yoshingiz  '+age;
-// }
-// alert(age())
+function age(){
+    let name= prompt('Isminginzni kiriting')
+    let now = +prompt('hozirgi yilni kiriting')
+    let old = +prompt ("tug'ilgan yilingiz")
+    let age = now - old;
+    return name + '  Yoshingiz  '+age;
+}
+alert(age())
 
